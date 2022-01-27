fdff89b4c119489370494a2dbd877015b17e1556
ruch0401 <ruch0401@gmail.com>
2022-01-26 17:57:53 -0800

    Added new test resouce

--- src/main/java/com/example/app/MyApp.java
+++ src/main/java/com/example/app/MyApp.java
@@ -1,10 +1,12 @@
 package com.example.app;
 
+import com.example.controllers.MyMessage;
 import com.example.controllers.MyResource;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class MyApp extends ResourceConfig {
     public MyApp() {
         register(MyResource.class);
+        register(MyMessage.class);
     }
 }
